8356cbc7e992b88e6ed33fe6c2c3fcc6a7bcfa19
katy@karstaway.com <katy@karstaway.com>
2018-08-26 08:37:02 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,17 +2,17 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-         :name => "Lord Montague"
+         :name => "Lord Montague",
          :age => "53"
         },
       :matriarch => {
-       :name => "Lady Montague" 
+       :name => "Lady Montague", 
        :age => "54".
         },
       :hero => {
-       :name of "Romeo",
-an :age of "15", and
-a :status of "alive".
+       :name => "Romeo",
+       :age =>"15", 
+       :status =>"alive".
         },
       :hero_friends => []
    }, 
